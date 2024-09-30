73f9ce5645fca1cdee65d366b02c73df7dee0788
Karim Buksh <karimkbgabol@gmail.com>
2024-09-30 22:14:37 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import Name from "./Name"
+export default function App(){
+  return <div>
+  <div>Hello world!</div>
+  <Name></Name>
+  <Name></Name>
+  <Name></Name>
+  <Name></Name>
+  </div>
+}
---
+++ src/Name.tsx
@@ -0,0 +1,3 @@
+export default function Name(){
+    return <div>My Name is Karim</div>
+}
